fa8e8d39007c65737361726532deec8ffa53ba72
angelalim1010 <angela.lim57@myhunter.cuny.edu>
2019-02-16 15:54:59 -0500

    thumbs

--- frontend/src/components/Thumbs.js
+++ frontend/src/components/Thumbs.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react'
+import '../styles/Thumbs.css';
 
 export default class Thumbs extends Component {
     constructor(props){
         super(props);
         this.state = {
-
+            thumbPercentage: '1'
         }
     }
+    saveColor = (e) => {
+        e.preventDefault();
+    }
+
     render() {
         return (
             <div>
+                <div className="body"> {this.state.thumbPercentage}% </div>
                 <div class="rating">
                     <div class="like grow">
                         <i class="fa fa-thumbs-up fa-3x like" aria-hidden="true"></i>
