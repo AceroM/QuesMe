9293987c141e42609dfeb1626fccbd3584201fc9
Leo <leo297747@gmail.com>
2019-02-16 14:57:22 -0500

    Added basic Login page

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,11 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-export default class Login extends Component {
-  render() {
-    return (
-      <div>
-        this is a login page
-      </div>
-    )
-  }
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            password: "",
+            tempusername: "",
+            temppassword: "",
+        };
+    }
+
+    handleChange = (event) => {
+      this.setState({
+        [event.target.name]: event.target.value
+      });
+    }
+
+    handleSubmit() {
+      //Will also need to pass username up (to Express?)
+      this.props.setLoggedIn();
+    }
+
+    render() {
+        return (
+            <div className="Login">
+                <form onSubmit={this.handleSubmit}>
+                    Username:&nbsp;
+                    <input
+                        name="username"
+                        type="text" 
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                    />
+                    <br></br>
+                    Password:&nbsp;
+                    <input
+                        name="password"
+                        type="text" 
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                    />
+                    <br></br>
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+        );
+      }
 }
+
+export default Login;
