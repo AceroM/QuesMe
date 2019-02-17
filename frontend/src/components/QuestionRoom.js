7c7fa952ccc1da34e272c90259e7ac0b72c4d68e
Shariar Kabir <shariar.k8@gmail.com>
2019-02-16 20:58:50 -0500

    add word bubble

--- frontend/src/components/QuestionRoom.js
+++ frontend/src/components/QuestionRoom.js
@@ -10,12 +10,21 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import LikeButton from "./LikeButton";
+import BubbleChart from '@weknow/react-bubble-chart-d3';
+
 
 export default class Home extends Component {
   constructor(props) {
     super();
     this.state = {
-      listOfQuestions: [{text: "Can I eat apples?", votes:0}]
+      listOfQuestions: [
+        { label: 'What is a linked list?', value: 34 },
+        { label: 'Why should we use a stack again?', value: 1 },
+        { label: 'How any bits are in a int again?', value: 1 },
+        { label: 'How to calculate derivative?', value: 23 },
+        { label: 'Can you go over question 1?', value: 25 },
+        { label: 'What is the runetime for #2?', value: 26 },
+        { label: 'Why didnt we use a deque? ', value: 26 }]
     }; 
   }
 
@@ -29,18 +38,30 @@ export default class Home extends Component {
   }
 
   incrementVote = (index) => {
-    console.log(index);
     let listOfQuestions = this.state.listOfQuestions;
-    listOfQuestions[index].votes = listOfQuestions[index].votes + 1
+    listOfQuestions[index].value = listOfQuestions[index].value + 1
     this.setState({
       listOfQuestions: listOfQuestions
     })
   }
 
-  decrementVote = (index) => {
-    console.log(index);
+
+  bubbleClick = (label) =>{
     let listOfQuestions = this.state.listOfQuestions;
-    listOfQuestions[index].votes = listOfQuestions[index].votes - 1
+    for (var i = 0; i < listOfQuestions.length ; i++){
+        if (listOfQuestions[i].label == label){
+            listOfQuestions[i].value = listOfQuestions[i].value + 1
+            this.setState({
+            listOfQuestions: listOfQuestions
+            })  
+        }
+
+    }
+  }
+
+  decrementVote = (index) => {
+    let listOfQuestions = this.state.listOfQuestions;
+    listOfQuestions[index].value = listOfQuestions[index].value - 1
     this.setState({
       listOfQuestions: listOfQuestions
     })
@@ -51,6 +72,17 @@ export default class Home extends Component {
     const rows = this.state.listOfQuestions;
     return (
       <div>
+          <div className="WordCloud">
+        <h1 className="WordCloud">Questions</h1>
+        <br />
+        <BubbleChart
+          width={800}
+          height={800}
+          fontFamily="Arial"
+          bubbleClickFun={this.bubbleClick}
+          data={this.state.listOfQuestions}
+        />
+      </div>
       <Paper className="Question-Paper">
       <Table className="Question-Table">
         <TableHead>
@@ -64,9 +96,9 @@ export default class Home extends Component {
         {rows.map((row, index) => (
             <TableRow key={index}>
               <TableCell component="th" scope="row">
-              {row.text}
+              {row.label}
               </TableCell>
-              <TableCell align="right">{row.votes}</TableCell>
+              <TableCell align="right">{row.value}</TableCell>
               <TableCell align="right"><Button size="small" color="primary" >
             <LikeButton link="https://image.flaticon.com/icons/png/128/126/126471.png" decrementVote = {this.decrementVote} incrementVote = {this.incrementVote} my_id = {index} my_index = {this.props.my_key} likes = {this.props.likes} style={{display: 'flex'}}/>
             <br />
