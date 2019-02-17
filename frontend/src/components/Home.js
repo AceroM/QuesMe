f735359c3bed5ac098b97cbc8ab3837710d7fab3
AceroM <miguel.acero81@myhunter.cuny.edu>
2019-02-16 22:01:46 -0500

    socket sucks

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -10,23 +10,21 @@ export default class Home extends Component {
     }
 
     render() {
-        const socket = socketIOClient("localhost:5000");
-        socket.emit('user_connect', this.props.username.toLowerCase());
         const { isLoggedIn, isTeacher, username, studentname } = this.props;
 
         return (
             <div className="Home">
-                { isLoggedIn ? (
+                {isLoggedIn ? (
                     // If logged in, check if teacher or student
                     isTeacher ? (
                         // If is teacher, display dashboard
                         <Dashboard username={username} />
                     ) : (
-                        //If is student, display classroom
-                        <Classroom studentname={studentname} />
-                    )
+                            //If is student, display classroom
+                            <Classroom studentname={studentname} />
+                        )
                 ) : (
-                    // If not logged in, redirect to login page
+                        // If not logged in, redirect to login page
                         <Redirect to="/login" />
                     )}
             </div>
